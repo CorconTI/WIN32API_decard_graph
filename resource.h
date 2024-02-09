b9ede98d7087aded3f4d31262ef2897bc76bc9bd
CorconTI <92674431+CorconTI@users.noreply.github.com>
2024-02-09 09:21:13 +0700

    Create resource.h

---
+++ resource.h
@@ -0,0 +1,29 @@
+
+// resource.h
+#ifndef RESOURCE_H
+#define RESOURCE_H
+
+#define IDM_EXIT 100 
+#define IDM_ABOUT 101
+#define IDM_COLOR1 102
+#define IDM_COLOR2 105
+#define IDM_SHOW1 103
+#define IDM_SHOW2 106
+#define IDM_OPEN 104
+#define IDM_SETUP 107
+#define IDM_LISTBOX 109
+#define ID_LIST 108
+
+#define IDC_STATIC -1
+#define IDC_XMAX 200
+#define IDC_XMIN 201
+#define IDC_XSTEP 202
+#define IDC_YMAX 300
+#define IDC_YMIN 301
+#define IDC_YSTEP 302
+#define IDC_XSHOW 303
+#define IDC_YSHOW 304
+#define IDC_MSTEP 306
+#define IDC_U0 308
+
+#endif
